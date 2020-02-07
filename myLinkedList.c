7c69de1aead4a399d1d1513a6d5f912fb52bc856
John Ramirez <jramirez5@pride.hofstra.edu>
2020-02-07 15:40:21 -0500

    working on deletenode function

--- myLinkedList.c
+++ myLinkedList.c
@@ -9,35 +9,38 @@ typedef struct Node{
 void insertNode(Node *head, int data);
 void printNode(Node *head, int data);
 void printList(Node *head);
+void deleteNode(Node *head, int data);
 
 int main(int argc, char *argv[]){
 
-	Node *head = malloc(sizeof(Node));
-	head->data = 55;
+	Node *head = (Node *)malloc(sizeof(Node));
+	head->data = NULL;
 	insertNode(head, 22);
-	printf("The data at current is: %d\n",head->data);
+	printf("The data at current is: %d\n",head->next->data);
 	insertNode(head, 56);
-	printf("The data at current is: %d\n",head->data);
+	printf("The data at current is: %d\n",head->next->data);
 	insertNode(head, 72);
-	printf("The data at current is: %d\n",head->data);
+	printf("The data at current is: %d\n",head->next->data);	
 	printList(head);
-	//printf("Head's data value is: %d\n",*head);
-	//printf("Head's data value is: %d\n",head->data);
+	printf("The value of head->next is: %d\n",head->next);
+	deleteNode(head, 22);
+	printList(head);
 	
 }
 
 //insertNode - inserts a new node to the beginning of the linked list
-void insertNode(Node *head, int data){	
-	if (head->next == NULL){	// if no nodes are in the list, add to the beginning
+void insertNode(Node *head, int data){
+	Node *current = head;	
+	if (current->next == NULL){	// if no nodes are in the list, add to the beginning
 		Node *newNode = (Node *)malloc(sizeof(Node));
-		head->next = newNode;
-		head->next->data = data;
+		current->next = newNode;
+		newNode->data = data;
 	}
 	else{	//there exist nodes in the list
 		Node *newNode = (Node *)malloc(sizeof(Node));
-		newNode->next = head->next;
-		head->next = newNode;
-		head->next->data = data;
+		newNode->next = current->next;
+		current->next = newNode;
+		newNode->data = data;
 	}
 }
 
@@ -55,10 +58,32 @@ void printList(Node *head){
 	Node *current = head;
 	printf("head->");
 	while(current->next != NULL){
-		printf("[%d]->",current->data);
+		printf("[%d]->",current->next->data);
 		current->next = current->next->next;
 	}
 	printf("Every node has been printed.\n");
+}
+
+void deleteNode(Node *head, int data){
+	Node *current = head;
+	printf("The address of head is: %d\n",head);
+	if(current->next == NULL){
+		printf("The list is empty. The value of head->next is:%d\n",current->next);
+	}
+	else{
+		while(current->next != NULL){	//while there is a node in the list
+			if(current->next->data == data){
+				if(current->next->next == NULL){
+					current->next = NULL;
+					printf("Deleted the node at the end of the list.");
+				}
+				else{
+					current->next = current->next->next;
+					printf("Deleted a node in the middle of the list.");
+				}
+			}		
+		}
+	}
 }
 
 /*Functions:
